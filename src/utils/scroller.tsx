a1d218c4837fa90cde5eb30e48a4643c9da08d2a
tahaamin2013 <tahaamindob2013@gmail.com>
2025-05-20 13:52:57 -0700

    fds

--- src/utils/scroller.tsx
+++ src/utils/scroller.tsx
@@ -1,5 +1,7 @@
 // utils.ts
 export function handleSmoothScroll(element: string) {
+  if (typeof document === "undefined") return; // ⛔️ Prevent on server
+
   const targetElement = document.getElementById(element);
   if (targetElement) {
     targetElement.scrollIntoView({
