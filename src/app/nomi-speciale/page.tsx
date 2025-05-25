f20d91927804daceb1b7c83aff2f21cb242e35ea
tahaamin2013 <tahaamindob2013@gmail.com>
2025-05-25 14:58:41 -0700

    fdsa

--- src/app/nomi-speciale/page.tsx
+++ src/app/nomi-speciale/page.tsx
@@ -5,7 +5,7 @@ export const metadata: Metadata = {
   title: "Nome Speciale - Generatore di Testo Fantasioso",
   description: "Crea un nome speciale con il nostro generatore di testo fantasioso. Personalizza e copia facilmente nomi unici e creativi.",
   alternates: {
-    canonical: "https://caratterispeciali10.it/nomi-speciali",
+    canonical: "https://caratterispeciali10.it/nomi-speciale",
   },
 };
 export default function Home() {
