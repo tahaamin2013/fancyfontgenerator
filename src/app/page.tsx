0518c7f7901805406bfd086b10f35a75c01daa1a
tahaamin2013 <tahaamindob2013@gmail.com>
2025-07-22 21:55:04 +0500

    fd

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -86,8 +86,7 @@ const FancyTextGenerator: React.FC = () => {
     <div className="min-h-screen bg-gradient-to-b from-white to-purple-50 dark:from-gray-900 dark:to-gray-800 overflow-hidden">
      <Head>
 
-        {/* WebSite Schema */}
-        <script
+         <script
           type="application/ld+json"
           dangerouslySetInnerHTML={{
             __html: JSON.stringify({
