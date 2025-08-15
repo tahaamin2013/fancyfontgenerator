debbac164899b8ff55d4869f5838bc08a373afda
tahaamin2013 <tahaamindob2013@gmail.com>
2025-08-15 16:11:04 -0700

    fds

--- next.config.js
+++ next.config.js
@@ -15,11 +15,12 @@ const nextConfig = {
             key: "X-Frame-Options",
             value: "SAMEORIGIN",
           },
-          {
-            key: "Content-Security-Policy",
-            value:
-              "default-src 'self'; script-src 'self' 'unsafe-inline' https://www.googletagmanager.com https://www.google-analytics.com; style-src 'self' 'unsafe-inline'; img-src 'self' data:; font-src 'self'; connect-src 'self' https://www.google-analytics.com; frame-ancestors 'self';",
-          },
+     {
+  key: "Content-Security-Policy",
+  value:
+    "default-src 'self'; script-src 'self' 'unsafe-inline' https://www.googletagmanager.com https://www.google-analytics.com; style-src 'self' 'unsafe-inline'; img-src 'self' data:; font-src 'self'; connect-src 'self' https://www.google-analytics.com https://pagead2.googlesyndication.com https://googleads.g.doubleclick.net https://www.google.com; frame-ancestors 'self';",
+},
+
           {
             key: "Referrer-Policy",
             value: "strict-origin-when-cross-origin",
