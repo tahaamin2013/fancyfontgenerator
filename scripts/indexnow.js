e3381bba08323b87ec9e0c6d11bae3c30e173f1b
tahaamin2013 <tahaamindob2013@gmail.com>
2025-08-21 21:40:21 -0700

    fd

--- scripts/indexnow.js
+++ scripts/indexnow.js
@@ -9,7 +9,27 @@ async function submitUrls() {
       key: "a185741da28c4b7a9ed494dcaec7bb29",
       keyLocation: "https://caratterispeciali10.it/a185741da28c4b7a9ed494dcaec7bb29.txt",
       urlList: [
-        "https://caratterispeciali10.it/chi-siamo"
+ "https://www.caratterispeciali10.it/caratteri-speciali",
+    "https://www.caratterispeciali10.it/chi-siamo",
+    "https://www.caratterispeciali10.it/contattaci",
+    "https://www.caratterispeciali10.it/disclaimer",
+    "https://www.caratterispeciali10.it/emojis",
+    "https://www.caratterispeciali10.it/font-corsivo",
+    "https://www.caratterispeciali10.it/generatore-testo-corsivo",
+    "https://www.caratterispeciali10.it/lista-de-atalhos-do-teclado-para",
+    "https://www.caratterispeciali10.it/nomi-speciale",
+    "https://www.caratterispeciali10.it",
+    "https://www.caratterispeciali10.it/privacy",
+    "https://www.caratterispeciali10.it/social/generatore-di-caratteri-per-facebook",
+    "https://www.caratterispeciali10.it/social/generatore-di-caratteri-per-instagram",
+    "https://www.caratterispeciali10.it/social/generatore-di-caratteri-per-nickname",
+    "https://www.caratterispeciali10.it/social/generatore-di-caratteri-per-tumblr",
+    "https://www.caratterispeciali10.it/social/generatore-di-caratteri-per-twitter",
+    "https://www.caratterispeciali10.it/social/generatore-di-caratteri-per-whatsapp",
+    "https://www.caratterispeciali10.it/special-character-table-in-html",
+    "https://www.caratterispeciali10.it/termini-e-condizioni",
+    "https://www.caratterispeciali10.it/text-characters-emoticons",
+    "https://www.caratterispeciali10.it/unicode-tavoli"
       ]
     })
   });
