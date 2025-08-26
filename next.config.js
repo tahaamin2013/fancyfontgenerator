866ab9f7df44919280638028219ef2c5079d5997
tahaamin2013 <tahaamindob2013@gmail.com>
2025-08-25 21:17:28 -0700

    fds

--- next.config.js
+++ next.config.js
@@ -2,33 +2,33 @@
 
 
 const nextConfig = {
-  async headers() {
-    return [
-      {
-        source: "/(.*)",
-        headers: [
-          {
-            key: "X-Content-Type-Options",
-            value: "nosniff",
-          },
-          {
-            key: "X-Frame-Options",
-            value: "SAMEORIGIN",
-          },
+  // async headers() {
+  //   return [
+  //     {
+  //       source: "/(.*)",
+  //       headers: [
+  //         {
+  //           key: "X-Content-Type-Options",
+  //           value: "nosniff",
+  //         },
+  //         {
+  //           key: "X-Frame-Options",
+  //           value: "SAMEORIGIN",
+  //         },
 
 
-          {
-            key: "Referrer-Policy",
-            value: "strict-origin-when-cross-origin",
-          },
-          {
-            key: "Strict-Transport-Security",
-            value: "max-age=31536000; includeSubDomains; preload",
-          },
-        ],
-      },
-    ];
-  },
+  //         {
+  //           key: "Referrer-Policy",
+  //           value: "strict-origin-when-cross-origin",
+  //         },
+  //         {
+  //           key: "Strict-Transport-Security",
+  //           value: "max-age=31536000; includeSubDomains; preload",
+  //         },
+  //       ],
+  //     },
+  //   ];
+  // },
 
 };
 
