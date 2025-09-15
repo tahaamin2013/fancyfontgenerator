51f577e24358420e0e347d18906f34a0f7ae57fb
tahaamin2013 <tahaamindob2013@gmail.com>
2025-09-15 00:41:52 -0700

    d

--- src/components/Homepage.tsx
+++ src/components/Homepage.tsx
@@ -232,6 +232,9 @@ const FancyTextGenerator: React.FC = () => {
                     </p>
                   </div>
 
+                  <script async data-cfasync="false" src="//pl27647015.revenuecpmgate.com/561945773a837ed34a8c131b672a147a/invoke.js"></script>
+<div id="container-561945773a837ed34a8c131b672a147a"></div>
+
                   <div className="bg-gray-50 dark:bg-gray-700 p-6 rounded-xl">
                     <div className="w-12 h-12 bg-purple-100 dark:bg-purple-900 rounded-full flex items-center justify-center mb-4">
                       <svg
