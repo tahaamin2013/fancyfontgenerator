57ba4d8b5085463517a608b856de8de9141a5220
tahaamin2013 <tahaamindob2013@gmail.com>
2025-09-16 12:31:09 -0700

    d

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -99,6 +99,10 @@ export default function RootLayout({
           <Providers>
                     <Toaster />
             {children}
+            <script async data-cfasync="false" src="//pl27647015.revenuecpmgate.com/561945773a837ed34a8c131b672a147a/invoke.js"></script>
+<div id="container-561945773a837ed34a8c131b672a147a"></div>
+
+
             <ToastContainer />
           </Providers>
           <Footer />
