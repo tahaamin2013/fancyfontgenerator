b82099cfc22743b6257f703f7eb0cdb6b19c0c41
tahaamin2013 <tahaamindob2013@gmail.com>
2025-09-17 11:25:00 -0700

    d

---
+++ src/components/nativebanner.tsx
@@ -0,0 +1,13 @@
+
+const Nativebanner = () => {
+  return (
+    <div>
+      <div className="flex justify-center my-6 w-full">
+        <script async data-cfasync="false" src="//pl27647015.revenuecpmgate.com/561945773a837ed34a8c131b672a147a/invoke.js"></script>
+        <div id="container-561945773a837ed34a8c131b672a147a"></div>
+      </div>
+    </div>
+  );
+};
+
+export default Nativebanner;
